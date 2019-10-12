094eb122c91d35a45bbcb90ab0c440268c51cfd5
simao <you@example.com>
2019-10-12 11:58:08 +0100

    secont commit

--- alias.c
+++ alias.c
@@ -15,16 +15,15 @@ t_aliaspkg *StoreAddrStruct(t_aliaspkg *addr)
 void PushToList(char *string, t_aliaspkg *data)
 {
 	t_alias *node;
-	char 	   **array;
+	int	i;
 	
-	(void)data;
-	(void)string;
 	if (!(node = (t_alias *)malloc(sizeof(t_alias))))
 		return ;
-	if (!(array = ft_strsplit(string, '=')))
-		return ;
-	node->shortcut = ft_strdup(array[0]);
-	node->cmd = ft_strdup(array[1]);
+	i = -1;
+	while (string[++i] && string[i] != '=')
+		;
+	node->shortcut = ft_strsub(string, 0, ++i);
+	node->cmd = ft_strsub(string, i, ft_strlen(string) - 1);
 	node->next = NULL;
 	if (!data->head_ref && !data->tail_ref)
 		data->head_ref = data->tail_ref = node;
@@ -41,20 +40,24 @@ void CreateAliasFile(t_aliaspkg *data)
 
 	if (!flag)
 	{
-		if (!(access("42hrc", F_OK) == 0))
-			data->fd = open("42hrc", O_WRONLY | O_CREAT, 0777);
+		if (!(access("42shrc", F_OK) == 0))
+			data->fd = open("42shrc", O_WRONLY | O_CREAT, 0777);
 		else
-			data->fd = open("42hrc", O_WRONLY);
+			data->fd = open("42shrc", O_WRONLY);
 		flag = 1;
 	}
 }
 
-void ImportAliasFileContent(t_aliaspkg *data)
+void ImportAliasFileContent()
 {
+	t_aliaspkg *data;
 	char	*line;
+	int	fd;
 
 	line = NULL;
-	while (get_next_line(data->fd, &line))
+	data = StoreAddrStruct(NULL);
+	fd = open("42shrc", O_RDONLY);
+	while (get_next_line(fd, &line))
 		PushToList(line, data);
 }
 
@@ -68,8 +71,29 @@ void PrintList()
 	while (curr)
 	{
 		ft_putendl(curr->shortcut);
+		ft_putendl(curr->cmd);
 		curr = curr->next;
 	}
+}
+
+void AliasMatched(char *shortcut, t_aliaspkg *data)
+{
+	t_alias *curr;
+
+	// loop through cmdlist;
+	//while ()
+//	{
+		curr = data->head_ref;
+		while (curr)
+		{
+			if (ft_strcmp(curr->shortcut, shortcut) == 0) // change shortcut with cmd in the cmdlist;
+			{
+				ft_putendl("here assign cmd in the list to the new value stored int curr->cmd");
+				break ; 
+			}
+			curr = curr->next;
+		}
+//	}
 }
 
 int main(int ac, char **av)
@@ -83,9 +107,12 @@ int main(int ac, char **av)
 	CreateAliasFile(data);
 	while (1)
 	{
+		ft_bzero(buff, 1024);
 		read(0, buff, sizeof(buff));
-		ImportAliasFileContent(data);
-		PushToList(buff, data);
+		ImportAliasFileContent();
+		PushToList(buff);
+		AliasMatched(buff);
+		RemoveAlias(buff);
 		PrintList();
 	}
 	return (0);
